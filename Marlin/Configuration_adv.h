709758edb81fd1ae7c21baf37b4c14ca46e459f7
deanar <rdeanar@gmail.com>
2021-02-03 23:24:57 +0300

    Set steps and current for direct extruder motor

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1936,8 +1936,8 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      1100
-    #define E0_MICROSTEPS    8
+    #define E0_CURRENT      450
+    #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
   #endif
